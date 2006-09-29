a3f9ed92acf032d59411c9b16b0008893eeb0741
halprin <halprin@38d01ca6-8e1e-0410-b48b-a0b07a665ce4>
2006-09-29 02:55:09 +0000

    initial

---
+++ mod_install/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  UT2k4 Mod Installer
+//
+//  Created by Peter Kendall on 7/17/06.
+//  Copyright __MyCompanyName__ 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
