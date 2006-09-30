a46700b4bbb695e9d3d85f21021756f59ed7c9f7
halprin <halprin@38d01ca6-8e1e-0410-b48b-a0b07a665ce4>
2006-09-30 07:02:43 +0000

    Added open dialog boxes to locate UT2k4 and the mod you want to install.  Made sure menu items said correct name of program.

--- mod_install/MainWindow.m
+++ mod_install/MainWindow.m
@@ -6,28 +6,59 @@
 {
 	NSButtonCell *zip=[zip_umod cellAtRow: 0 column: 0];
 	NSButtonCell *umod=[zip_umod cellAtRow: 0 column: 1];
-	NSString *path=@"";
-	if([zip state]==1)
+	NSString *path=@"-1";
+	if([zip state]==1)  //ZIP is selected in the Radio control
 	{
 		NSLog(@"ZIP is selected so display open dialog for .zip files");
-		path=@"ChaosUT2.zip";
+		//setup and display the open dialog box
+		NSArray *fileTypes=[NSArray arrayWithObject: @"zip"];
+		NSOpenPanel *oPanel=[NSOpenPanel openPanel];
+		int result=[oPanel runModalForDirectory: nil file: nil types: fileTypes];
+		if(result==NSOKButton)
+		{
+			NSArray *file=[oPanel filenames];
+			path=[file objectAtIndex: 0];
+		}
 	}
-	else if([umod state]==1)
+	else if([umod state]==1)  //UMOD is selected in the Radio control
 	{
 		NSLog(@"UMOD is selected so display open dialog for .ut4mod files");
-		path=@"ChaosUT2.ut4mod";
+		//setup and display the open dialog box
+		NSArray *fileTypes=[NSArray arrayWithObjects: @"ut4mod", @"umod", nil];
+		NSOpenPanel *oPanel=[NSOpenPanel openPanel];
+		int result=[oPanel runModalForDirectory: nil file: nil types: fileTypes];
+		if(result==NSOKButton)
+		{
+			NSArray *file=[oPanel filenames];
+			path=[file objectAtIndex: 0];
+		}
 	}
-	else
+	else  //This should never execute, if so, BUG!
 	{
 		NSLog(@"Bugzor!");
 	}
-	[mod_path setStringValue: path];
+	if(path!=@"-1")  //If the user clicked Cancel in the open dialog box
+	{
+		//set the mod location text box to the path
+		[mod_path setStringValue: path];
+	}
 }
 
 - (IBAction)findUT:(id)sender
 {
-	NSString *path=@"Kendall/Users/halprin/Desktop/Peter/Unreal Tournament 2004/Unreal Tournament 2004.app/";
-	[ut_path setStringValue: path];
+	NSString *path=@"-1";
+	NSLog(@"display open dialog for .app files");
+	//setup and display the open dialog box
+	NSArray *fileTypes=[NSArray arrayWithObject: @"app"];
+	NSOpenPanel *oPanel=[NSOpenPanel openPanel];
+	int result=[oPanel runModalForDirectory: nil file: nil types: fileTypes];
+	if(result==NSOKButton)
+	{
+		NSArray *file=[oPanel filenames];
+		path=[file objectAtIndex: 0];
+		//set the UT2k4 location text box to the path
+		[ut_path setStringValue: path];
+	}
 }
 
 - (IBAction)installMod:(id)sender
