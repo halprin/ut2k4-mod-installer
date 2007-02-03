96e4bfb52b71a5324d94901effb90b66a32a0067
halprin <halprin@38d01ca6-8e1e-0410-b48b-a0b07a665ce4>
2007-02-03 00:47:56 +0000

    New NSTask engine is in place that does not use my explicitly making my own thread.  Only zip installs work at this time.

--- trunk/Progress.h
+++ trunk/Progress.h
@@ -6,10 +6,14 @@
 {
     IBOutlet NSProgressIndicator *progress_bar;
     IBOutlet NSTextField *status_text;
+	IBOutlet NSTextView *log;
 	NSString *ut_path;
 	NSString *mod_path;
 	NSString *zip_umod;
 	BOOL inst_done;
+	NSTask *task;
+	NSMutableArray *elements;
+	int install_index;
 }
 -(void) windowDidLoad;
 -(void) setUT: (NSString*) path;
@@ -18,5 +22,7 @@
 -(void) windowLoaded: (NSNotification*) notification;
 -(void) installDone: (NSNotification*) notification;
 -(void) start: (NSTimer*) timer;
+-(void)reader: (NSNotification*) notification;
+-(void) end: (NSTimer*) timer;
 -(void)dealloc;
 @end
